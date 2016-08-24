4741ed898b6b5ac612bb0d2d16e73b737362bb21
SufferProgrammer <pijipirma@gmail.com>
2016-08-24 17:23:47 +0700

    3rd revision, make interactive email for forgot password and update the executable files

--- controller/controller.py
+++ controller/controller.py
@@ -1,5 +1,6 @@
 import smtplib
 
+
 class Controller():
     def __init__(self):
         self.username = 'pypijidevel@gmail.com'
@@ -7,10 +8,12 @@ class Controller():
     
     def sendMailController(self, mailRecipient, emailMessage):
 
+        message = 'Hei howdy users\nThis is yours apps developer, you have been lost yours password right ?\nBelow is yours password\n\n"%s"\n\nDont lost yours password again next time okay ;D' %(emailMessage)
+
         self.server = smtplib.SMTP('smtp.gmail.com:587')
         self.server.ehlo()
         self.server.starttls()
         self.server.login(self.username, self.password)
         self.fromPypiji = 'pypijidevel@gmail.com'
-        self.server.sendmail(self.fromPypiji, mailRecipient, emailMessage)
+        self.server.sendmail(self.username, mailRecipient, message)
         
--- forgot_pw.py
+++ forgot_pw.py
@@ -22,12 +22,15 @@ class ForgotPw(QtGui.QWidget, forgot_ui.Ui_Form):
         if username and emailAddr != '':
             dataGetPasswdWithUnameDetailed = dbcontroller.DBControl()
             dataFetchDBRes = dataGetPasswdWithUnameDetailed.forgetPasswdUserSpecified(str(username), str(emailAddr))
-            password = ''.join(dataFetchDBRes)
-            mailMessage = "Hai Client, this is from your's apps developer. You have been forgot password and your's password is '%s' for now" %(password)
+            passwd = ''.join(dataFetchDBRes)
             emailSendPasswdBot = controller.Controller()
-            emailSendPasswdBot.sendMailController(str(emailAddr), mailMessage)
+            emailSendPasswdBot.sendMailController(str(emailAddr), passwd)
 
             QtGui.QMessageBox.information(self, 'Success', "Password has been sent to email, please check your's mailbox")
+
+            self.login = login.Login()
+            self.login.show()
+            self.hide()
         else:
             QtGui.QMessageBox.critical(self, 'Error', 'Email and username is empty')
 
