b07c72f57a25f449b52b8bad52bc131cfd006210
SufferProgrammer <pijipirma@gmail.com>
2016-08-25 10:22:22 +0700

    serving user his profile, re-arrange UI structure in register, and member windows, also adding user image for touch(experimental)

--- database/database.sql
+++ database/database.sql
@@ -1,1 +1,4 @@
-CREATE TABLE users(id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, username VARCHAR(30), password VARCHAR(30), email VARCHAR(100), user_level INT NOT NULL)
+DROP TABLE users;
+CREATE TABLE users(id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, username VARCHAR(30), password VARCHAR(30), email VARCHAR(100), image BLOB, user_level INT NOT NULL);
+INSERT INTO users(username, password, email, user_level) VALUES('admin', 'admin', 'admin@minad.min', '1');
+INSERT INTO users(username, password, email, user_level) VALUES('developer', 'developer', 'oniioniichan@gmail.com', '2');
